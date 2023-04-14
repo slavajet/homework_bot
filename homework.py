e5308648e9d7464017255504eefd2fb6d7664949
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-04-14 22:54:59 +0600

    написал функцию get api answer

--- homework.py
+++ homework.py
@@ -12,20 +12,20 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
-ENDPOINT = "https://praktikum.yandex.ru/api/user_api/homework_statuses/"
-HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
+ENDPOINT = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_VERDICTS = {
-    "approved": "Ревьюеру всё понравилось, можно приступать к следующему уроку.",
-    "rejected": "К сожалению в работе нашлись ошибки.",
-    "reviewing": "Работа находится на проверке у ревьюера.",
+    'approved': 'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
+    'rejected': 'К сожалению в работе нашлись ошибки.',
+    'reviewing': 'Работа находится на проверке у ревьюера.',
 }
 
 
 def check_tokens():
     """Проверяет доступность переменных окружения."""
     if not PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        raise ValueError("Не все переменные окружения доступны")
+        raise ValueError('Не все переменные окружения доступны')
 
 
 def send_message(bot, message):
@@ -34,7 +34,15 @@ def send_message(bot, message):
 
 
 def get_api_answer(timestamp):
-    ...
+    """Запрос к API."""
+    params = {'from_date': timestamp}
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+        response.raise_for_status()
+    except Exception as e:
+        print(f'Возникла ошибка при запросе к API: {e}')
+        return None
+    return response.json()
 
 
 def check_response(response):
@@ -44,7 +52,7 @@ def check_response(response):
 def parse_status(homework):
     ...
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    return f'Изменился статус проверки работы '{homework_name}'. {verdict}'
 
 
 def main():
