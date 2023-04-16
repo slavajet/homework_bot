48ca59f9c36205838a59a5fbe82bc5b39a60431d
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-04-16 19:39:40 +0600

    Заставил себя анотировать код

--- homework.py
+++ homework.py
@@ -1,41 +1,42 @@
 import os
 import time
+
 import requests
 import telegram
 from dotenv import load_dotenv
-
+from typing import Dict, Optional
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN: Optional[str] = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN: Optional[str] = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID: Optional[str] = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+RETRY_PERIOD: int = 600
+ENDPOINT: str = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS: Dict[str, str] = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-HOMEWORK_VERDICTS = {
+HOMEWORK_VERDICTS: Dict[str, str] = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
 
-def check_tokens():
+def check_tokens() -> None:
     """Проверяет доступность переменных окружения."""
     if not PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
         raise ValueError('Не все переменные окружения доступны')
 
 
-def send_message(bot, message):
+def send_message(bot, message: str) -> None:
     """Отправляет сообщение в телеграм."""
     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
-def get_api_answer(timestamp):
+def get_api_answer(timestamp: int) -> Dict[str, list[Dict[str, str]]]:
     """Запрос к API."""
-    params = {'from_date': timestamp}
+    params: Dict[str, int] = {'from_date': timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         response.raise_for_status()
@@ -45,7 +46,7 @@ def get_api_answer(timestamp):
     return response.json()
 
 
-def check_response(api_answer):
+def check_response(api_answer: Dict[str, list[Dict[str, str]]]) -> None:
     """Проверяет корректность ответа от API."""
     if "homeworks" not in api_answer:
         raise ValueError('Отсутствует поле homeworks в ответе API.')
@@ -57,7 +58,7 @@ def check_response(api_answer):
         raise ValueError('Поле current_date в ответе API должно быть числом.')
 
 
-def parse_status(homework):
+def parse_status(homework: Dict[str, str]) -> str:
     """Получает статус работы."""
     status = homework["status"]
     homework_name = homework["homework_name"]
@@ -65,7 +66,7 @@ def parse_status(homework):
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def main():
+def main() -> None:
     """Основная логика работы бота."""
     check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
